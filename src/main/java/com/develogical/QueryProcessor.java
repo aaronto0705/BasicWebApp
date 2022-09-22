3e34e0432f0fccd2737ecabc2d566ff317dc9ad7
Aaron To <AaronTo@macintosh.wifi.local.cmu.edu>
2022-09-22 10:28:07 -0400

    Updated API Key

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -5,7 +5,7 @@ public class QueryProcessor {
     public String process(String query) {
         if (query.toLowerCase().contains("pokemon")) {
             return "Pokémon is a series of video games developed by Game Freak and published " +
-                   " by Nintendo and The Pokémon Company under the Pokémon media franchise.";
+                   "by Nintendo and The Pokémon Company under the Pokémon media franchise.";
         }
         return "";
     }
