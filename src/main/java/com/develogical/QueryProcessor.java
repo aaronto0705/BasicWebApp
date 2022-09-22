307d2acc27362be3570d83dfdc7318f94ce095b2
Aaron To <AaronTo@macintosh.wifi.local.cmu.edu>
2022-09-21 23:40:50 -0400

    Changed QueryProcessor to check for builds

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -3,10 +3,9 @@ package com.develogical;
 public class QueryProcessor {
 
     public String process(String query) {
-        if (query.toLowerCase().contains("shakespeare")) {
-            return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
-                    "English poet, playwright, and actor, widely regarded as the greatest " +
-                    "writer in the English language and the world's pre-eminent dramatist.";
+        if (query.toLowerCase().contains("pokemon")) {
+            return "Pokémon is a series of video games developed by Game Freak and published " +
+                   " by Nintendo and The Pokémon Company under the Pokémon media franchise.";
         }
         return "";
     }
